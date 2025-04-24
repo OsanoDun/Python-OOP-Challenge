57305599d400adb67a7894ff275ffd4a781fd053
OsanoDun <osanodun@gmail.com>
2025-04-24 04:06:08 +0300

    Python OOP Challenge Group-346 Submission

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+from pet import Pet
+
+# Create a Pet object
+my_pet = Pet("Namibia")
+
+# Call Pet methods to test functionality
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.train("fetch")
+my_pet.train("catch")
+my_pet.show_tricks()
+my_pet.get_status()
